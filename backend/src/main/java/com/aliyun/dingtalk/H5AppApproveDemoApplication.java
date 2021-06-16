f12708a4e405b277e81ae6682c20c1bbd2abbeba
wanzhiqiang <15638940333@163.com>
2021-06-16 20:32:43 +0800

    initial project

---
+++ backend/src/main/java/com/aliyun/dingtalk/H5AppApproveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aliyun.dingtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5AppApproveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H5AppApproveDemoApplication.class, args);
+    }
+
+}

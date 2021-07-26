c9ffa8953d566f54abe26149b25010a25edbe43e
张蕊 <dudu996@126.com>
2021-07-26 18:00:59 +0800

    feat：'最后一步'

--- fronted/src/List.js
+++ fronted/src/List.js
@@ -4,32 +4,6 @@ import {domain} from "./App";
 import {Form, Input, Button, Space, Table} from "antd";
 import {MinusCircleOutlined, PlusOutlined} from '@ant-design/icons';
 
-// const buttonStyle = {height:'60px',margin: '10px', padding: '10px', fontsize:'18px'};
-
-// class TrData extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//     }
-
-//     render() {
-//         return (
-//             this.props.items.map((item) => {
-//                 const name = item.formComponentValues[0].name;
-//                 const result = item.result;
-//                 const approveUserid = item.approverUserids[0];
-//                 return (
-//                     <tr className="text-center">
-//                         <td>{name}</td>
-//                         <td>{result}</td>
-//                         <td>{approveUserid}</td>
-//                     </tr>
-//                 )
-//             })
-//         )
-//     }
-// }
-
 class List extends React.Component {
 
     constructor(props) {
@@ -65,7 +39,7 @@ class List extends React.Component {
                 if (response.data.success) {
                     alert("审批提交成功")
                 } else {
-                    alert("审批创建失败")
+                    alert(response.data.errorMsg)
                 }
                 // 根据instanceId获取实例详情
                 // axios.get(domain + '/process/instance/' + response.data.data)
@@ -83,86 +57,6 @@ class List extends React.Component {
                 console.log(error);
             });
     };
-
-    // response = {
-    //     "success": true,
-    //     "errorCode": null,
-    //     "errorMsg": null,
-    //     "data": [
-    //         {
-    //             "approverUserids": [
-    //                 "043217290519980938"
-    //             ],
-    //             "attachedProcessInstanceIds": [],
-    //             "bizAction": "NONE",
-    //             "businessId": "202107261018000327301",
-    //             "ccUserids": [
-    //                 "043217290519980938"
-    //             ],
-    //             "createTime": "2021-07-26T02:18:33.000+00:00",
-    //             "finishTime": null,
-    //             "formComponentValues": [
-    //                 {
-    //                     "componentType": "TextField",
-    //                     "extValue": null,
-    //                     "id": "物品用途",
-    //                     "name": "物品用途",
-    //                     "value": "测试物品领用"
-    //                 },
-    //                 {
-    //                     "componentType": "TableField",
-    //                     "extValue": "{\"statValue\":[],\"componentName\":\"TableField\"}",
-    //                     "id": "TableField-MINGXI",
-    //                     "name": "物品明细",
-    //                     "value": "[{\"rowValue\":[{\"componentType\":\"TextField\",\"label\":\"物品名称\",\"value\":\"测试物品领用-电脑\",\"key\":\"物品名称\"},{\"componentType\":\"NumberField\",\"label\":\"数量\",\"value\":\"3\",\"key\":\"数量\"},{\"componentType\":\"DDPhotoField\",\"label\":\"图片\",\"value\":\"[\\\"https://img.alicdn.com/imgextra/i4/O1CN01Y2sejp27k1TCmDENg_!!6000000007834-2-tps-1924-918.png\\\",\\\"https://img.alicdn.com/imgextra/i4/O1CN015zvwqo1WDNSZzfk4O_!!6000000002754-2-tps-2878-1520.png\\\"]\",\"key\":\"DDPhotoField_1D4MDW1RQTZ40\"}]},{\"rowValue\":[{\"componentType\":\"TextField\",\"label\":\"物品名称\",\"value\":\"测试物品领用-电脑1\",\"key\":\"物品名称\"},{\"componentType\":\"NumberField\",\"label\":\"数量\",\"value\":\"2\",\"key\":\"数量\"}]}]"
-    //                 },
-    //                 {
-    //                     "componentType": "TextareaField",
-    //                     "extValue": null,
-    //                     "id": "领用详情",
-    //                     "name": "领用详情",
-    //                     "value": "领用详情1"
-    //                 },
-    //                 {
-    //                     "componentType": "DDPhotoField",
-    //                     "extValue": null,
-    //                     "id": "图片",
-    //                     "name": "图片",
-    //                     "value": "null"
-    //                 }
-    //             ],
-    //             "mainProcessInstanceId": null,
-    //             "operationRecords": [
-    //                 {
-    //                     "attachments": null,
-    //                     "date": "2021-07-26T02:18:32.000+00:00",
-    //                     "operationResult": "NONE",
-    //                     "operationType": "START_PROCESS_INSTANCE",
-    //                     "remark": null,
-    //                     "userid": "043217290519980938"
-    //                 }
-    //             ],
-    //             "originatorDeptId": "-1",
-    //             "originatorDeptName": "浩倡测试企业申请认证",
-    //             "originatorUserid": "043217290519980938",
-    //             "result": "",
-    //             "status": "RUNNING",
-    //             "tasks": [
-    //                 {
-    //                     "activityId": "b5ebc027-2f09-47b8-9b20-34c7ac4f02a3",
-    //                     "createTime": "2021-07-26T02:18:33.000+00:00",
-    //                     "finishTime": null,
-    //                     "taskResult": "NONE",
-    //                     "taskStatus": "RUNNING",
-    //                     "taskid": "69699095515",
-    //                     "url": "?procInsId=57b2e166-2897-40f5-b71b-698d7cc5ef40&taskId=69699095515&businessId=202107261018000327301",
-    //                     "userid": "043217290519980938"
-    //                 }
-    //             ],
-    //             "title": "万志强提交的测试物品领用"
-    //         }
-    //     ]
-    // }
 
     getTableRowData = () => {
         // 获取审批列表
@@ -194,7 +88,6 @@ class List extends React.Component {
             .catch(error => {
                 alert(JSON.stringify(error))
             })
-        // console.log(error.message)
     };
 
     onFinish = (values) => {
@@ -289,35 +182,9 @@ class List extends React.Component {
                 {this.state.auditList ? '提交申领审批' : '查看审批记录'}
             </a>
             {
-                this.state.auditList && <Table columns={this.columns} dataSource={this.state.items.formComponentValues}
-                                               hideOnSinglePage={true}/>
+                this.state.auditList && <Table columns={this.columns} dataSource={this.state.items.formComponentValues}/>
             }
         </div>
-        // if (!this.state.isLoaded) {
-        //     return (<div>
-        //         <button style={buttonStyle} onClick={this.goodsCollectionAndApprove}>领用并提交审批</button>
-        //         <button style={buttonStyle} onClick={this.getTableRowData}>获取提交的审批信息</button>
-        //     </div>)
-        // } else {
-        //     return (<div>
-        //         <button style={buttonStyle} onClick={this.goodsCollectionAndApprove}>领用并提交审批</button>
-        //         <button style={buttonStyle} onClick={this.getTableRowData}>获取提交的审批信息</button>
-        //         <table className="table table-bordered">
-        //             <thead>
-        //             <tr>
-        //                 <th>物品用途</th>
-        //                 <th>审批状态</th>
-        //                 <th>审批人</th>
-        //             </tr>
-        //             </thead>
-        //             <tbody>
-
-        //             <TrData items={this.state.items}/>
-
-        //             </tbody>
-        //         </table>
-        //     </div>)
-        // }
     }
 
     columns = [
@@ -331,7 +198,6 @@ class List extends React.Component {
             dataIndex: 'more',
             key: 'more',
             render: value => {
-                console.log(value, '=====')
                 return value.map(text => {
                     return <p>{text}</p>
                 })
@@ -345,19 +211,17 @@ class List extends React.Component {
             title: '流程状态',
             dataIndex: 'status',
             key: 'status',
-            render: status => {
-                console.log(this.state.items);
-                console.log("status: " + status);
+            render: (status='RUNNING') => {
                 switch (status) {
                     case 'NEW':
-                        <>新建</>;
+                        return <>新建</>;
                     case 'RUNNING':
-                        <>审批中</>;
+                        return <>审批中</>;
                     case 'COMPLETED':
-                        <>完成</>
+                        return <>完成</>
                     case 'CANCELED':
-                        <>取消</>
-                        break;
+                        return <>取消</>;
+                    break;
                 }
 
             }
